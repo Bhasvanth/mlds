1226b40fe01df59ab465ff6682c203d54356527b
Bhasvanth Yarla <bhasvanth@gmail.com>
2017-03-21 01:06:17 -0700

    fixed bugs

--- samples/Pandas/basic_operations.py
+++ samples/Pandas/basic_operations.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-#print ("Python Version" + sys.version)
-#print ("Pandas Version" + pd.__version__)
-#print ("Matplotlib Version" + matplotlib.__version__)
+print ("Python Version" + sys.version)
+print ("Pandas Version" + pd.__version__)
+print ("Matplotlib Version" + matplotlib.__version__)
 
 names = ['Bob','Jessica','Mary','John','Mel']
 births = [968, 155, 77, 578, 973]
@@ -23,7 +23,7 @@ df = pd.DataFrame(data=babyDataSet, columns=["Names", "Birth"])
 
 df.to_csv("births1880.csv", index=False, header=False)
 
-csvLocation = r'/root/mlds/samples/Pandas/births1880.csv'
+csvLocation = r'./births1880.csv'
 #df = pd.read_csv(csvLocation, header=None)
 df = pd.read_csv(csvLocation, names=["Names", "Births"])
 
@@ -45,7 +45,7 @@ df["Births"].plot()
 maxName = df["Names"][df["Births"] == df["Births"].max()].values
 
 txt = str(maxValue) + " - "   + maxName
-plt.annotate(Text, xy=(1, MaxValue), xytext=(8, 0), 
+plt.annotate(txt, xy=(1, maxValue), xytext=(8, 0), 
                  xycoords=('axes fraction', 'data'), textcoords='offset points')
 
 print ("most popular name")
